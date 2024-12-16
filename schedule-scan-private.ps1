2b4e3715c0c11e3988e964e465722ab313afaaf3
NeronNymus <neron.academico@gmail.com>
2024-12-15 18:11:29 -0600

    update

--- schedule-scan-private.ps1
+++ schedule-scan-private.ps1
@@ -1,4 +1,10 @@
 # Get the path to Python executable
 $python_path = (Get-Command python).Definition
 
-echo $python_path
+# Define the action
+$action = New-ScheduledTaskAction -Execute "$python_path" -Argument 'C:\Users\jesus\Other\scan_private\scan_private2.py'
+
+$t1 = New-ScheduledTaskTrigger -Daily -At 06:10pm
+$t2 = New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes 1) -RepetitionDuration (New-TimeSpan -Hours 1) -At 06:10pm
+$t1.Repetition = $t2.Repetition
+Register-ScheduledTask -Action $action -Trigger $t1 -TaskName "RunPrivate" -Force
