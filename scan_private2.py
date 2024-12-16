ae988dc9b12ff39128a821cad79949b89c695096
NeronNymus <neron.academico@gmail.com>
2024-12-15 19:05:02 -0600

    update

--- scan_private2.py
+++ scan_private2.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     if os.name == 'posix':
         project_path = os.getcwd()  # Execute it in the current project path
     elif os.name == 'nt':
-        project_path = "C:\\Users\\jesus\Other\\scan_private" # Hardcoded path
+        project_path = "C:\\Users\\jesus\\Other\\scan_private" # Hardcoded path
 
     scan_dir = os.path.join(project_path,"scans/active3")
     scan_result = scan_nmap(scan_dir)
