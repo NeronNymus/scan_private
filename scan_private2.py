8ba8922ad64e95da91d1c8d094a3b56fa828d8fc
NeronNymus <neron.academico@gmail.com>
2024-12-16 12:43:07 -0600

    update

--- scan_private2.py
+++ scan_private2.py
@@ -100,17 +100,7 @@ if __name__ == "__main__":
     filename = "exports2"
     load_env_vars(filename)
 
-
-    #print("DB_HOST:", os.getenv("DB_HOST"))
-    #print("DB_NAME:", os.getenv("DB_NAME"))
-    #print("DB_USER:", os.getenv("DB_USER"))
-    #print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
-    #print("DB_PORT:", os.getenv("DB_PORT"))
-
-
-    #sys.exit(0)
     # Example usage
-
     if os.name == 'posix':
         project_path = os.getcwd()  # Execute it in the current project path
     elif os.name == 'nt':
