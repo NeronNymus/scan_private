127fc2bb8fb18f63434c841e67afc137cabd6776
NeronNymus <neron.academico@gmail.com>
2024-12-17 13:35:10 -0600

    update

--- schedule-scan-private.ps1
+++ schedule-scan-private.ps1
@@ -4,8 +4,12 @@ $python_path = (Get-Command python).Definition
 # Define the action
 $action = New-ScheduledTaskAction -Execute "$python_path" -Argument 'C:\Users\jesus\Other\scan_private\scan_private2.py'
 #$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command `"& '$python_path' 'C:\Users\jesus\Other\scan_private\scan_private2.py'`""
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" `
+    -Argument "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -Command `"& '$python_path' 'C:\Users\jesus\Other\scan_private\scan_private2.py'`""
 
-$t1 = New-ScheduledTaskTrigger -Daily -At 13:00pm
+
+#$t1 = New-ScheduledTaskTrigger -Daily -At 13:00pm
+$t1 = New-ScheduledTaskTrigger -Daily -At 13:00pm -AsJob
 #$t2 = New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes 60) -RepetitionDuration (New-TimeSpan -Hours 24) -At 19:00pm
 $t2 = New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes 1) -RepetitionDuration (New-TimeSpan -Hours 24) -At 13:00pm
 $t1.Repetition = $t2.Repetition
